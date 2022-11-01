901d4bdb4fe8cfbef05c5e76c7f003792c42d82e
NeiHau <yamanekonotsleep1210@gmail.com>
2022-11-01 14:04:29 +0900

    init

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,33 @@
-import 'package:first_app/page/event_editing_page%20copy.dart';
 import 'package:first_app/view/calendar_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:month_year_picker/month_year_picker.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  final String title = 'Calendar Events App';
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        MonthYearPickerLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('ja'),
+      ],
       debugShowCheckedModeBanner: false,
-      title: title,
-      home: const MainPage(),
-    );
-  }
-}
-
-class MainPage extends StatelessWidget {
-  const MainPage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Calendar'),
-        centerTitle: true,
+      title: '自作カレンダー',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      body: CustomCalendar(),
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(
-          Icons.add,
-          color: Colors.white,
-        ),
-        onPressed: () => Navigator.of(context).push(
-          MaterialPageRoute(builder: (context) => const EventEditingPage()),
-        ),
-      ),
+      home: const CalendarPage(),
     );
   }
 }
---
+++ lib/view/calendar.dart
@@ -0,0 +1,275 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:month_year_picker/month_year_picker.dart';
+
+class Calendar extends StatefulWidget {
+  final int weekDay;
+  final Color? color;
+
+  const Calendar({this.weekDay = 7, this.color, Key? key}) : super(key: key);
+
+  @override
+  _CalendarState createState() => _CalendarState();
+}
+
+class _CalendarState extends State<Calendar> {
+  final List<String> _weekName = ['月', '火', '水', '木', '金', '土', '日'];
+  late DateTime selectedDate;
+  DateTime _now = DateTime.now();
+  final int _monthDuration = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    selectedDate = _now;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        currentMonth(),
+        dayOfWeek(),
+        Expanded(
+          child: calendar(),
+        ),
+      ],
+    );
+  }
+
+  Container currentMonth() {
+    return Container(
+      color: Colors.white,
+      height: 45,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          /*
+          GestureDetector(
+            child: const Icon(
+              Icons.arrow_left,
+              size: 30,
+              color: Colors.black,
+            ),
+            onTap: () {
+              _monthDuration--;
+              setState(() {});
+            },
+          ),
+          */
+          Text(
+            DateFormat('yyyy年M月').format(
+              DateTime(_now.year, _now.month + _monthDuration, 1),
+            ),
+            style: const TextStyle(
+              fontSize: 20.0,
+              color: Colors.black,
+            ),
+          ),
+          GestureDetector(
+            child: const Icon(
+              Icons.arrow_drop_down,
+              size: 30,
+              color: Colors.black,
+            ),
+            onTap: () {
+              setState(() {
+                selectDate(context: context, locale: 'ja');
+              });
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget dayOfWeek() {
+    List<Widget> weekList = [];
+    int weekIndex = widget.weekDay - 1; //初期値-1
+    int counter = 0;
+    while (counter < 7) {
+      weekList.add(
+        Expanded(
+          child: Container(
+            decoration: BoxDecoration(color: Colors.grey.shade200),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 5),
+              child: Text(
+                _weekName[weekIndex % 7],
+                textAlign: TextAlign.center,
+                style: const TextStyle(fontSize: 12.0, color: Colors.black),
+              ),
+            ),
+          ),
+        ),
+      );
+      weekIndex++;
+      counter++;
+    }
+    return Row(
+      children: weekList,
+    );
+  }
+
+  Widget calendar() {
+    List<Widget> list = [];
+
+    DateTime firstDayOfTheMonth =
+        DateTime(_now.year, _now.month + _monthDuration, 1);
+    int monthLastNumber =
+        DateTime(firstDayOfTheMonth.year, firstDayOfTheMonth.month + 1, 1)
+            .add(const Duration(days: -1))
+            .day;
+    List<Widget> listCache = [];
+    for (int i = 1; i <= monthLastNumber; i++) {
+      listCache.add(
+        Expanded(
+          child: buildCalendarItem(i,
+              DateTime(firstDayOfTheMonth.year, firstDayOfTheMonth.month, i)),
+        ),
+      );
+
+      if (DateTime(firstDayOfTheMonth.year, firstDayOfTheMonth.month, i)
+                  .weekday ==
+              newLineNumber(startNumber: widget.weekDay) ||
+          i == monthLastNumber) {
+        int repeatNumber = 7 - listCache.length;
+        for (int j = 0; j < repeatNumber; j++) {
+          if (DateTime(firstDayOfTheMonth.year, firstDayOfTheMonth.month, i)
+                  .day <=
+              7) {
+            listCache.insert(
+              0,
+              Expanded(
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: buildBorder(),
+                  ),
+                ),
+              ),
+            );
+          } else {
+            listCache.add(
+              Expanded(
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: buildBorder(),
+                  ),
+                ),
+              ),
+            );
+          }
+        }
+
+        list.add(
+          Row(
+            children: listCache,
+          ),
+        );
+        listCache = [];
+      }
+    }
+
+    return Column(
+      children: list,
+    );
+  }
+
+  Border buildBorder() =>
+      Border.all(color: widget.color ?? Theme.of(context).primaryColor);
+
+  int newLineNumber({required int startNumber}) {
+    if (startNumber == 1) return 7;
+    return startNumber - 1;
+  }
+
+  Widget buildCalendarItem(int i, DateTime cacheDate) {
+    bool isToday = (selectedDate.difference(cacheDate).inDays == 0) &&
+        (selectedDate.day == cacheDate.day);
+    if (isToday) {
+      return Container(
+        alignment: Alignment.topCenter,
+        /*
+        decoration: BoxDecoration(
+          border: buildBorder(),
+        ),
+        */
+        child: Container(
+          margin: const EdgeInsets.all(3),
+          alignment: Alignment.center,
+          width: 30,
+          height: 30,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: (isToday)
+                ? widget.color ?? Theme.of(context).primaryColor
+                : Colors.transparent,
+          ),
+          child: Text(
+            '$i',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+                fontSize: 14.0,
+                color: (isToday) ? Colors.white : Colors.black,
+                fontWeight: FontWeight.bold),
+          ),
+        ),
+      );
+    }
+
+    return GestureDetector(
+      behavior: HitTestBehavior.opaque,
+      child: Container(
+        alignment: Alignment.topCenter,
+        //decoration: BoxDecoration(border: buildBorder()),
+        child: Container(
+          margin: const EdgeInsets.all(3),
+          alignment: Alignment.center,
+          width: 30,
+          height: 30,
+          child: Text(
+            '$i',
+            textAlign: TextAlign.center,
+            style: const TextStyle(fontSize: 14.0),
+          ),
+        ),
+      ),
+      onTap: () {
+        //print('${DateFormat('yyyy年M月d日').format(cacheDate)}が選択されました');
+        selectedDate = cacheDate;
+        setState(() {});
+      },
+    );
+  }
+
+  //
+  static Color dateColor(DateTime date) {
+    switch (date.weekday) {
+      case DateTime.sunday:
+        return Colors.red;
+
+      case DateTime.saturday:
+        return Colors.blue;
+
+      default:
+        return Colors.black;
+    }
+  }
+
+  selectDate({required BuildContext context, String? locale}) async {
+    final localeObj = locale != null ? Locale(locale) : null;
+    final selectedDate = await showMonthYearPicker(
+      context: context,
+      initialDate: _now,
+      firstDate: DateTime(DateTime.now().year - 1),
+      lastDate: DateTime(DateTime.now().year + 100),
+      locale: localeObj,
+    );
+
+    if (selectedDate == null) return;
+
+    setState(() {
+      _now = selectedDate;
+    });
+  }
+}
